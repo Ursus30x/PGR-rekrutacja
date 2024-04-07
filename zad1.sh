b5df3eb7476f28861334b55afabb24a1fc83d875
Ursus30x <jakubszymczyk20@gmail.com>
2024-04-07 17:58:15 +0200

    zad1 dodane

---
+++ zad1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if [ -e /proc/sys/fs/binfmt_misc/WSLInterop ]; then         #jesli jest WSL
+   echo "Amount of variables: $(env | grep -E "BASH|SHELL" | wc -l)"
+   env | grep -E "BASH|SHELL"
+else                                                        #jesli jest native Linux
+    SERVICE_MAN="$(cat /proc/1/comm )"
+    if [ $SERVICE_MAN = "systemd" ]; then             #dla systemu dzialajacego pod systemd
+        echo "$(systemctl status sshd.service)"
+    elif [ $SERVICE_MAN = "init" ]; then             #dla systemu dzialajacego pod sysVinit
+        echo "$(service sshd.service status)"
+    elif [ $SERVICE_MAN = "upstart" ]; then          #dla systemu dzialajacego pod upstart
+        echo "$(initctl status sshd.service)"
+    else                                                    # dla systemu dzialajacego pod innym service managerem
+        VAR=$(top -n 1 -b | grep "sshd")
+        if [ -n "$VAR" ]; then
+            echo "sshd service is running"
+        else 
+            echo "sshd service is disabled"
+        fi
+    fi
+fi
